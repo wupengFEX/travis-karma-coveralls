03fad3e77876f75bf0128a4d8e66a2cc0a4a2eee
wupengFEX <smartfutureplayer@gmail.com>
2017-07-12 16:20:34 +0800

    first ci

---
+++ src/jqueryExtend.js
@@ -0,0 +1,7 @@
+/**
+ * @author smart [wupeng_xg@163.com]
+ * @fileoverview The Library provide some function that jquery hasn't complicated!
+ */
+
+function aa () {
+}
---
+++ test/jqueryExtend.js
@@ -0,0 +1,5 @@
+describe("Jquery Method", function () {
+    it("htmlUnWrap", function (done) {
+    	done();
+    });
+});
